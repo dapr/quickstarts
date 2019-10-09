753382d34c2ca1c66980702d14155c96e95959ca
Aman Bhardwaj <amanbha@microsoft.com>
2019-10-08 19:36:02 -0700

    Adding license header and updating to MIT license.

--- 2.hello-kubernetes/python/app.py
+++ 2.hello-kubernetes/python/app.py
@@ -1,3 +1,8 @@
+# ------------------------------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------------------------------
+
 import time
 import requests
 import os
--- 4.pub-sub/react-form/client/src/serviceWorker.js
+++ 4.pub-sub/react-form/client/src/serviceWorker.js
@@ -1,3 +1,8 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
