b5b493008de475820f7821284d56eaabcc1a3e03
Ryan <ryanvolum@users.noreply.github.com>
2019-10-19 08:17:45 -0700

    Reconfigured samples to use env vars for Dapr port instead of hardcoded ports (#69)
    * Updated sample 1 to follow env variable convention
    
    * Updated node app to follow env var conventions
    
    * updated to follow env var conventions
    
    * reflected env var changes
    
    * updated to follow consistent env var convention
    
    * updated based on env var changes
    
    * updated to follow env var convention
    
    * updated to follow env var convention
    
    * Fixed subscriber bug
    
    * Undid port change in readme
    
    * Undid port change

--- 5.bindings/pythonapp/app.py
+++ 5.bindings/pythonapp/app.py
@@ -7,9 +7,7 @@ import time
 import requests
 import os
 
-dapr_port = os.getenv("DAPR_HTTP_PORT")
-if dapr_port is None:
-    dapr_port = 3500
+dapr_port = os.getenv("DAPR_HTTP_PORT", 3500)
 
 dapr_url = "http://localhost:{}/v1.0/bindings/sample-topic".format(dapr_port)
 n = 0
