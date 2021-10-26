543bd8618892c90fb29c88417b02cd1386842f3d
Amulya Varote <amulyavarote@Amulyas-MacBook-Pro.local>
2021-10-26 15:48:17 -0700

    Added java CheckoutService

---
+++ service_invocation/java/CheckoutService/src/main/java/com/service/CheckoutService/CheckoutServiceApplication.java
@@ -0,0 +1,13 @@
+package com.service.CheckoutService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutServiceApplication.class, args);
+	}
+
+}
