f1483b1800d8c1d19aab8d3c1ea8261d74e276f2
Amulya Varote <amulyavarote@Amulyas-MacBook-Pro.local>
2021-10-26 15:46:16 -0700

    Added java EventService

---
+++ service_invocation/java/EventService/src/test/java/com/service/event/EventServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
