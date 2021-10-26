a1a8d49ef523412833cac26e68d0daaddcc3b033
Amulya Varote <amulyavarote@Amulyas-MacBook-Pro.local>
2021-10-26 12:13:00 -0700

    First Commit

---
+++ csharp/EventService/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace EventService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           while(true) {
+                System.Threading.Thread.Sleep(5000);
+                Random random = new Random();
+                int orderId = random.Next(1,1000);
+                String uri = "https://localhost:6001/order/" + orderId;
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(uri);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage result = client.GetAsync(uri).Result;
+                Console.WriteLine("Order processed for order id " + orderId);
+                Console.WriteLine(result);
+		    }
+        }
+    }
+}
---
+++ python/Order.py
@@ -0,0 +1,5 @@
+class Order:
+
+    def __init__(self, orderName: str, orderNum: int) -> None:
+        self.orderName = orderName
+        self.orderNum = orderNum
