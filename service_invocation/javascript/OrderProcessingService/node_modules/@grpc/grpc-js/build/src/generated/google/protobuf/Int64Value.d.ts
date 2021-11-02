63beaaa434a97bb9d6324daf47191cf8c907caaa
Amulya Varote <amulyavarote@Amulyas-MacBook-Pro.local>
2021-11-02 12:42:59 -0700

    Remove ignored files

--- service_invocation/javascript/OrderProcessingService/node_modules/@grpc/grpc-js/build/src/generated/google/protobuf/Int64Value.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="long" />
-import type { Long } from '@grpc/proto-loader';
-export interface Int64Value {
-    'value'?: (number | string | Long);
-}
-export interface Int64Value__Output {
-    'value': (string);
-}
--- service_invocation/javascript/OrderProcessingService/node_modules/dapr-client/types/Etag.type.d.ts
+++
@@ -1,3 +0,0 @@
-export declare type IEtag = {
-    value: string;
-};
