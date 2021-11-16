8bab2bbb70d196ea84d84327665399941e91debc
Amulya Varote <amulyavarote@Amulyas-MacBook-Pro.local>
2021-11-16 05:48:02 -0800

    Added secrets management examples

---
+++ secrets_management/csharp/OrderProcessingService/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Text.Json;
+
+namespace EventService
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string SECRET_STORE_NAME = "localsecretstore";
+            using var client = new DaprClientBuilder().Build();
+            var secret = await client.GetSecretAsync(SECRET_STORE_NAME, "secret");
+            Console.WriteLine($"Result: {string.Join(", ", secret)}");
+            secret = await client.GetSecretAsync(SECRET_STORE_NAME, "secret");
+            Console.WriteLine($"Result for bulk: {string.Join(", ", secret.Keys)}");
+        }
+    }
+}
---
+++ secrets_management/go/OrderProcessingService/go.mod
@@ -0,0 +1,16 @@
+module dapr_example
+
+go 1.17
+
+require (
+	github.com/dapr/dapr v1.5.0 // indirect
+	github.com/dapr/go-sdk v1.2.0 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/genproto v0.0.0-20201204160425-06b3db808446 // indirect
+	google.golang.org/grpc v1.38.0 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
