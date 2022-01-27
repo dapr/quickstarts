fa01abbc0e509a0884975786abc6e335b589c049
Amulya Varote <amulyavarote@Amulyas-MacBook-Pro.local>
2022-01-27 14:13:29 -0800

    Added http type service invocation
    Signed-off-by: Amulya Varote <amulyavarote@Amulyas-MacBook-Pro.local>

--- service_invocation/go/OrderProcessingService/OrderProcessingService.go
+++ service_invocation/go/OrderProcessingService/OrderProcessingService.go
@@ -1,36 +1,41 @@
 package main
 
 import (
-	"context"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"math/rand"
-	"time"
+	"net/http"
+	"os"
 	"strconv"
-	dapr "github.com/dapr/go-sdk/client"
 
+	"github.com/gorilla/mux"
 )
 
-type Order struct {
-	orderName string
-	orderNum  string
+func getOrder(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	orderId := rand.Intn(1000-1) + 1
+	daprPort := "3602"
+	daprUrl := "http://localhost:" + daprPort + "/v1.0/invoke/checkout/method/checkout/" + strconv.Itoa(orderId)
+	response, err := http.Get(daprUrl)
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	result, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Order requested: " + strconv.Itoa(orderId))
+	log.Println("Result: ")
+	log.Println(result)
 }
 
 func main() {
-	for i := 0; i < 10; i++ {
-		time.Sleep(5000)
-		orderId := rand.Intn(1000-1) + 1
-		client, err := dapr.NewClient()
-		if err != nil {
-			panic(err)
-		}
-		defer client.Close()
-		ctx := context.Background()
-
-		result, err := client.InvokeMethod(ctx, "checkoutservice", "checkout/" + strconv.Itoa(orderId), "get")
-
-		log.Println("Order requested: " + strconv.Itoa(orderId))
-		log.Println("Result: ")
-		log.Println(result)
-	}
+	r := mux.NewRouter()
+	r.HandleFunc("/order", getOrder).Methods("GET")
+	http.ListenAndServe(":6001", r)
 }
-
