ce39ec97f747a82c9b74051610f80f32852749ac
Paul Yuknewicz <paulyuk@microsoft.com>
2022-01-31 01:55:06 -0800

    Next iteration of python pub/sub
    Signed-off-by: Amulya Varote <amulyavarote@Amulyas-MacBook-Pro.local>

---
+++ pub_sub/python/checkout/app.py
@@ -0,0 +1,21 @@
+from dapr.clients import DaprClient
+import json
+import time
+import random
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+while True:
+    order = {"orderid": random.randint(1, 1000)}
+
+    with DaprClient() as client:
+        result = client.publish_event(  # publish an event using Dapr pub-sub
+            pubsub_name="order_pub_sub",
+            topic_name="orders",
+            data=json.dumps(order),
+            data_content_type="application/json",
+        )
+
+    logging.info("Published data: " + json.dumps(order))
+    time.sleep(1)
