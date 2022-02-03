74d4914e055f5f7131da138984a86a7cb7335da4
Xavier Geerinck <xavier.geerinck@gmail.com>
2022-02-03 08:59:37 +0100

    Adapt to DAPR_ and fix --dapr-grpc to --dapr-http
    Signed-off-by: Xavier Geerinck <xavier.geerinck@gmail.com>
    Signed-off-by: Amulya Varote <amulyavarote@Amulyas-MacBook-Pro.local>

--- pub_sub/javascript/checkout/index.js
+++ pub_sub/javascript/checkout/index.js
@@ -1,11 +1,11 @@
 import { DaprClient } from 'dapr-client';
 import { v4 as uuidv4 } from 'uuid';
 
-const SIDECAR_HOST = process.env.SIDECAR_HOST || "127.0.0.1";
-const SIDECAR_PORT = process.env.SIDECAR_PORT || 3500;
+const DAPR_HOST = process.env.DAPR_HOST || "127.0.0.1";
+const DAPR_PORT = process.env.DAPR_PORT || 3500;
 
 async function main() {
-  const client = new DaprClient(SIDECAR_HOST, SIDECAR_PORT);
+  const client = new DaprClient(DAPR_HOST, DAPR_PORT);
 
   while (true) {
     await client.pubsub.publish("order_pub_sub", "orders", { orderId: uuidv4() });
--- pub_sub/javascript/order-processor/index.js
+++ pub_sub/javascript/order-processor/index.js
@@ -2,13 +2,13 @@
 // Checkout will do the pub sub
 import { DaprServer } from 'dapr-client';
 
-const SIDECAR_HOST = process.env.SIDECAR_HOST || "127.0.0.1";
-const SIDECAR_PORT = process.env.SIDECAR_PORT || 3500;
+const DAPR_HOST = process.env.DAPR_HOST || "127.0.0.1";
+const DAPR_PORT = process.env.DAPR_PORT || 3500;
 const SERVER_HOST = process.env.SERVER_HOST || "127.0.0.1";
 const SERVER_PORT = process.env.SERVER_PORT || 5000;
 
 async function main() {
-  const server = new DaprServer(SERVER_HOST, SERVER_PORT, SIDECAR_HOST, SIDECAR_PORT);
+  const server = new DaprServer(SERVER_HOST, SERVER_PORT, DAPR_HOST, DAPR_PORT);
   server.pubsub.subscribe("order_pub_sub", "orders", (data) => console.log(data));
   await server.start();
 }
