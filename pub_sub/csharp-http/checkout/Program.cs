d6311350d893bbe18c5306b1cd09101f84ccfcba
Paul Yuknewicz <paulyuk@microsoft.com>
2022-02-06 02:47:04 -0800

    Minimal .NET 6 code for http post to dapr pub/sub

--- pub_sub/csharp-http/checkout/Program.cs
+++ pub_sub/csharp-http/checkout/Program.cs
@@ -1,18 +1,23 @@
-﻿using System;
-using Dapr.Client;
+﻿using System.Text;
+using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
-using System.Net.Http;
+
+var baseURL = (Environment.GetEnvironmentVariable("BASE_URL") ?? "http://localhost") + ":" + (Environment.GetEnvironmentVariable("DAPR_HTTP_PORT") ?? "3500"); //reconfigure cpde to make requests to Dapr sidecar
+const string PUBSUBNAME = "order_pub_sub";
+const string TOPIC = "orders";
+Console.WriteLine($"Publishing to baseURL: {baseURL}, Pubsub Name: {PUBSUBNAME}, Topic: {TOPIC} ");
 
 var httpClient = new HttpClient();
+httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 
-while(true) {
+while (true) {
     Random random = new Random();
     var order = new Order(random.Next(1,1000));
-    using var client = new DaprClientBuilder().Build();
+    var orderJson = JsonSerializer.Serialize<Order>(order);
+    var content = new StringContent(orderJson, Encoding.UTF8, "application/json");
 
-    //Publish a message using Dapr pub/sub
-    await client.PublishEventAsync("order_pub_sub", "orders", order);
+    //Publish an event/message using Dapr PubSub via HTTP Post
+    var response = httpClient.PostAsync($"{baseURL}/v1.0/publish/{PUBSUBNAME}/{TOPIC}", content);
     Console.WriteLine("Published data: " + order);
 
     await Task.Delay(TimeSpan.FromSeconds(1));
