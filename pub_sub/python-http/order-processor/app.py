c9951877c34387c7b0350777a3fa9caf59ad966d
Paul Yuknewicz <paulyuk@microsoft.com>
2022-02-06 13:57:44 -0800

    add python-http
    Signed-off-by: Amulya Varote <amulyavarote@Amulyas-MacBook-Pro.local>

---
+++ pub_sub/python-http/order-processor/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from cloudevents.http import from_http
+import json
+
+app = Flask(__name__)
+
+
+# Dapr will send pub/sub events to routes defined in subscriptions
+@app.route('/dapr/subscribe', methods=['GET'])
+def subscribe():
+    subscriptions = [{'pubsubname': 'order_pub_sub',
+                      'topic': 'orders', 'route': 'orders'}]
+    return jsonify(subscriptions)
+
+
+# Dapr subscription routes orders topic to this route
+@app.route('/orders', methods=['POST'])
+def orders_subscriber():
+    event = from_http(request.headers, request.get_data())
+    print('Received message "{}" on topic "{}"'.format(
+          event.data['orderid'], event['topic']), flush=True)
+    return json.dumps({'success': True}), 200, {
+        'ContentType': 'application/json'}
+
+
+app.run(port=5001)
