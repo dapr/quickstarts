919574729a71f5870b24aab52b9511f9bf6c843a
Paul Yuknewicz <paulyuk@microsoft.com>
2022-02-06 01:30:51 -0800

    Draft commit for csharp-http quickstart -- uses no Dapr SDK, only http
    Signed-off-by: Paul Yuknewicz <paulyuk@microsoft.com>

---
+++ pub_sub/csharp-http/checkout/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Dapr.Client;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+var httpClient = new HttpClient();
+
+while(true) {
+    Random random = new Random();
+    var order = new Order(random.Next(1,1000));
+    using var client = new DaprClientBuilder().Build();
+
+    //Publish a message using Dapr pub/sub
+    await client.PublishEventAsync("order_pub_sub", "orders", order);
+    Console.WriteLine("Published data: " + order);
+
+    await Task.Delay(TimeSpan.FromSeconds(1));
+}
+
+public record Order([property: JsonPropertyName("orderId")] int OrderId);
