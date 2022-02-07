9bff60a8426035b8cf7afad25b2b85f9f7d14cf9
Paul Yuknewicz <paulyuk@microsoft.com>
2022-02-07 08:13:41 -0800

    Node pub-sub consistency fixes with other langs
    Signed-off-by: Amulya Varote <amulyavarote@Amulyas-MacBook-Pro.local>

--- pub_sub/javascript/order-processor/index.js
+++ pub_sub/javascript/order-processor/index.js
@@ -1,15 +1,16 @@
-// Order Processing = Server which processes items
-// Checkout will do the pub sub
 import { DaprServer } from 'dapr-client';
 
-const DAPR_HOST = process.env.DAPR_HOST || "127.0.0.1";
+const DAPR_HOST = process.env.DAPR_HOST || "localhost";
 const DAPR_PORT = process.env.DAPR_PORT || 3500;
-const SERVER_HOST = process.env.SERVER_HOST || "127.0.0.1";
-const SERVER_PORT = process.env.SERVER_PORT || 5000;
+const SERVER_HOST = process.env.SERVER_HOST || "localhost";
+const SERVER_PORT = process.env.SERVER_PORT || 5001;
 
 async function main() {
   const server = new DaprServer(SERVER_HOST, SERVER_PORT, DAPR_HOST, DAPR_PORT);
+
+  //Dapr subscription routes orders topic to this route
   server.pubsub.subscribe("order_pub_sub", "orders", (data) => console.log(data));
+  
   await server.start();
 }
 
