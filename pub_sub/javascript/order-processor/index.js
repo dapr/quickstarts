b58a6a50fb929cdec111e382c30c4a3422f705b4
Paul Yuknewicz <paulyuk@microsoft.com>
2022-02-07 11:00:56 -0800

    Using official DAPR_HTTP_PORT

--- pub_sub/javascript/order-processor/index.js
+++ pub_sub/javascript/order-processor/index.js
@@ -1,12 +1,12 @@
 import { DaprServer } from 'dapr-client';
 
 const DAPR_HOST = process.env.DAPR_HOST || "localhost";
-const DAPR_PORT = process.env.DAPR_PORT || 3500;
+const DAPR_HTTP_PORT = process.env.DAPR_HTTP_PORT || 3500;
 const SERVER_HOST = process.env.SERVER_HOST || "localhost";
 const SERVER_PORT = process.env.SERVER_PORT || 5001;
 
 async function main() {
-  const server = new DaprServer(SERVER_HOST, SERVER_PORT, DAPR_HOST, DAPR_PORT);
+  const server = new DaprServer(SERVER_HOST, SERVER_PORT, DAPR_HOST, DAPR_HTTP_PORT);
 
   // ßDapr subscription routes orders topic to this route
   server.pubsub.subscribe("order_pub_sub", "orders", (data) => {
