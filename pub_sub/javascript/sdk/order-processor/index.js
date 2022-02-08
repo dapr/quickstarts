9af25db5cfb2e963880f653749243c8590e9f292
Nick Greenfield <nigreenf@microsoft.com>
2022-02-07 20:48:35 -0800

    Add http example for JavaScript pubsub
    Signed-off-by: Nick Greenfield <nigreenf@microsoft.com>
    Signed-off-by: Amulya Varote <amulyavarote@Amulyas-MacBook-Pro.local>

--- pub_sub/javascript/sdk/order-processor/index.js
+++ pub_sub/javascript/sdk/order-processor/index.js
@@ -1,18 +1,16 @@
 import { DaprServer } from 'dapr-client';
 
-const DAPR_HOST = process.env.DAPR_HOST || "localhost";
-const DAPR_HTTP_PORT = process.env.DAPR_HTTP_PORT || 3500;
-const SERVER_HOST = process.env.SERVER_HOST || "localhost";
+const DAPR_HOST = process.env.DAPR_HOST || "http://localhost";
+const DAPR_HTTP_PORT = process.env.DAPR_HTTP_PORT || "3501";
+const SERVER_HOST = process.env.SERVER_HOST || "127.0.0.1";
 const SERVER_PORT = process.env.SERVER_PORT || 5001;
 
 async function main() {
   const server = new DaprServer(SERVER_HOST, SERVER_PORT, DAPR_HOST, DAPR_HTTP_PORT);
 
   // Dapr subscription routes orders topic to this route
-  server.pubsub.subscribe("order_pub_sub", "orders", (data) => {
-    console.log("Subscriber received : " + JSON.stringify(data))
-  });
-  
+  server.pubsub.subscribe("order_pub_sub", "orders", (data) => console.log("Subscriber received: " + JSON.stringify(data)));
+
   await server.start();
 }
 
