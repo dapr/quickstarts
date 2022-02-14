cf52eab0e5b16acd813bb675c99db9e2040975b6
Pravin Pushkar <ppushkar@microsoft.com>
2022-02-14 15:47:59 +0530

    Incorporating review comments
    Signed-off-by: Pravin Pushkar <ppushkar@microsoft.com>

--- pub_sub/java/sdk/order-processor/src/main/java/com/service/controller/OrderProcessingServiceController.java
+++ pub_sub/java/sdk/order-processor/src/main/java/com/service/controller/OrderProcessingServiceController.java
@@ -3,10 +3,11 @@ package com.service.controller;
 import io.dapr.Topic;
 import io.dapr.client.domain.CloudEvent;
 
-import org.springframework.http.HttpStatus;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,19 +17,24 @@ import reactor.core.publisher.Mono;
 @RestController
 public class OrderProcessingServiceController {
 
-    private static final Logger log = LoggerFactory.getLogger(OrderProcessingServiceController.class);
+    private static final Logger logger = LoggerFactory.getLogger(OrderProcessingServiceController.class);
 
     @Topic(name = "orders", pubsubName = "order_pub_sub")
-    @PostMapping(path = "/orders")
-    public Mono<ResponseEntity> getCheckout(@RequestBody(required = false) CloudEvent<String> cloudEvent) {
+    @PostMapping(path = "/orders", consumes = MediaType.ALL_VALUE)
+    public Mono<ResponseEntity> getCheckout(@RequestBody(required = false) CloudEvent<Order> cloudEvent) {
         return Mono.fromSupplier(() -> {
             try {
-                log.info("Subscriber received: " + cloudEvent.getData());
-                return new ResponseEntity<>("successful",
-                        HttpStatus.OK);
+                logger.info("Subscriber received: " + cloudEvent.getData().getOrderId());
+                return ResponseEntity.ok("SUCCESS");
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         });
     }
+}
+
+@Getter
+@Setter
+class Order {
+    private int orderId;
 }
