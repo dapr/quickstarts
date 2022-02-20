59064706dc297d02f06a22d9135fc5e97e511bb9
Paul Yuknewicz <paulyuk@microsoft.com>
2022-02-20 00:32:59 -0800

    Migrated distributed calculator from `request` with pipe to `axios` with service invoke.
    Signed-off-by: Paul Yuknewicz <paulyuk@microsoft.com>

--- distributed-calculator/react-calculator/server.js
+++ distributed-calculator/react-calculator/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
-const request = require('request');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = 8080;
 const daprPort = process.env.DAPR_HTTP_PORT || 3500;
@@ -19,30 +23,89 @@ The following routes forward requests (using pipe) from our React client to our 
 */
 
 app.post('/calculate/add', async (req, res) => {
-  const addUrl = `${daprUrl}/addapp/method/add`;
-  req.pipe(request(addUrl)).pipe(res);
+  const appUrl = `${daprUrl}/addapp/method/add`;
+
+  // Dapr invoke add app
+  console.log('Adding: ', req.body)
+  appResponse = await axios.post(appUrl, req.body);
+
+  // Return expected string result to client
+  const result = String(appResponse.data);
+  console.log('Result: ', result);
+
+  res.send(result); 
 });
 
 app.post('/calculate/subtract', async (req, res) => {
-  const subtractUrl = `${daprUrl}/subtractapp/method/subtract`;
-  req.pipe(request(subtractUrl)).pipe(res);
+  const appUrl = `${daprUrl}/subtractapp/method/subtract`;
+  
+  // Dapr invoke subtract app
+  console.log('Substracting: ', req.body)
+  appResponse = await axios.post(appUrl, req.body);
+
+  // Return expected string result to client
+  const result = String(appResponse.data);
+  console.log('Result: ', result);
+
+  res.send(result); 
 });
 
 app.post('/calculate/multiply', async (req, res) => {
-  const multiplyUrl = `${daprUrl}/multiplyapp/method/multiply`;
-  req.pipe(request(multiplyUrl)).pipe(res);
+  const appUrl = `${daprUrl}/multiplyapp/method/multiply`;
+
+  // Dapr invoke multiply app
+  console.log('Multiplying: ', req.body)
+  appResponse = await axios.post(appUrl, req.body);
+
+  // Return expected string result to client
+  const result = String(appResponse.data);
+  console.log('Result: ', result);
+
+  res.send(result);
 });
 
 app.post('/calculate/divide', async (req, res) => {
-  const divideUrl = `${daprUrl}/divideapp/method/divide`;
-  req.pipe(request(divideUrl)).pipe(res);
+  const appUrl = `${daprUrl}/divideapp/method/divide`;
+
+  // Dapr invoke divide app
+  console.log('Dividing: ', req.body)
+  appResponse = await axios.post(appUrl, req.body);
+
+  // Return expected string result to client
+  const result = String(appResponse.data);
+  console.log('Result: ', result);
+
+  res.send(result);
 });
 
 // Forward state retrieval to Dapr state endpoint
-app.get('/state', async (req, res) => req.pipe(request(`${stateUrl}/calculatorState`)).pipe(res));
+app.get('/state', async (req, res) => {
+
+  // Getting Dapr state
+  apiResponse = await axios.get(`${stateUrl}/calculatorState`);
+  console.log('Getting state: ', 'calculatorState');
+
+  // Return expected decimal result to client
+  const result = apiResponse.data;
+  console.log('Get state result: ', result);
+
+  res.send(result);
+});
 
 // Forward state persistence to Dapr state endpoint
-app.post('/persist', async (req, res) => req.pipe(request(stateUrl)).pipe(res));
+app.post('/persist', async (req, res) => {
+  //req.pipe(request(stateUrl)).pipe(res);
+
+  // Getting Dapr state
+  apiResponse = await axios.post(stateUrl, req.body);
+  console.log('Saving state: ', req.body)
+
+  // Return expected string result to client
+  const result = String(apiResponse.data);
+  console.log('State save returned: ', result);
+
+  res.send(result);
+});
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'client/build')));
