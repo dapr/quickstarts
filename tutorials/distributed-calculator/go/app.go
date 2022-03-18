0b0e3f7c7f366db6f175a9695fd86c4d3abbe459
Amulya Varote <amulyavarote@Amulyas-MacBook-Pro.local>
2022-03-18 11:11:18 -0700

    Renamed workflows and changes licenses
    Signed-off-by: Amulya Varote <amulyavarote@Amulyas-MacBook-Pro.local>

--- tutorials/distributed-calculator/go/app.go
+++ tutorials/distributed-calculator/go/app.go
@@ -1,22 +1,30 @@
-// ------------------------------------------------------------
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-// ------------------------------------------------------------
+//
+// Copyright 2021 The Dapr Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 
 package main
- 
+
 import (
 	"encoding/json"
-    "log"
-	"net/http"
 	"fmt"
-	
+	"log"
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
 type Operands struct {
-    OperandOne float32 `json:"operandOne,string"`
-    OperandTwo float32 `json:"operandTwo,string"`
+	OperandOne float32 `json:"operandOne,string"`
+	OperandTwo float32 `json:"operandTwo,string"`
 }
 
 func add(w http.ResponseWriter, r *http.Request) {
@@ -27,10 +35,10 @@ func add(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(fmt.Sprintf("%s%f%s%f", "Adding ", operands.OperandOne, " to ", operands.OperandTwo))
 	json.NewEncoder(w).Encode(operands.OperandOne + operands.OperandTwo)
 }
- 
+
 func main() {
 	router := mux.NewRouter()
-	
+
 	router.HandleFunc("/add", add).Methods("POST", "OPTIONS")
 	log.Fatal(http.ListenAndServe(":6000", router))
 }
