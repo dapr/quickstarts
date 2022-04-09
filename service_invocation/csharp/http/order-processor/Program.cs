963dfe02bfc0218d0cb21b919b4342f0789da793
David Fowler <davidfowl@gmail.com>
2022-04-09 13:05:22 -0700

    Update Program.cs
    Use model binding

--- service_invocation/csharp/http/order-processor/Program.cs
+++ service_invocation/csharp/http/order-processor/Program.cs
@@ -1,15 +1,16 @@
-using System.Text.Json.Serialization;
-
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment()) {app.UseDeveloperExceptionPage();}
+if (app.Environment.IsDevelopment()) 
+{
+    app.UseDeveloperExceptionPage();
+}
 
-app.MapPost("/orders", async context => {
-    var data = await context.Request.ReadFromJsonAsync<Order>();
-    Console.WriteLine("Order received : " + data);
-    await context.Response.WriteAsync(data.ToString());
+app.MapPost("/orders", async (Order order) 
+{
+    Console.WriteLine("Order received : " + order);
+    return order.ToString();
 });
 
 await app.RunAsync();
