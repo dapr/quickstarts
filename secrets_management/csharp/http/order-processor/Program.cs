825a50410314d7fef9783e4641d2a36624fed977
Amulya Varote <amulyavarote@microsoft.com>
2022-04-18 14:21:46 -0700

    Added secrets management examples with the workflow
    Signed-off-by: Amulya Varote <amulyavarote@microsoft.com>

---
+++ secrets_management/csharp/http/order-processor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var baseURL = (Environment.GetEnvironmentVariable("BASE_URL") ?? "http://localhost") + ":" 
++ (Environment.GetEnvironmentVariable("DAPR_HTTP_PORT") ?? "3500");
+const string DAPR_SECRET_STORE = "localsecretstore";
+const string SECRET_NAME = "secret";
+
+var httpClient = new HttpClient();
+httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+// Get secret from a local secret store
+var secret = await httpClient.GetStringAsync($"{baseURL}/v1.0/secrets/{DAPR_SECRET_STORE}/{SECRET_NAME}");
+Console.WriteLine("Fetched Secret: " + secret);
---
+++ secrets_management/javascript/http/order-processor/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const DAPR_HOST = process.env.DAPR_HOST || "http://localhost";
+const DAPR_HTTP_PORT = process.env.DAPR_HTTP_PORT || "3500";
+const DAPR_SECRET_STORE = "localsecretstore";
+const SECRET_NAME = "secret";
+
+async function main() {
+  // Get secret from a local secret store
+  await axios.get(`${DAPR_HOST}:${DAPR_HTTP_PORT}/v1.0/secrets/${DAPR_SECRET_STORE}/${SECRET_NAME}`)
+  .then(function (secret) {
+    console.log("Fetched Secret: ", secret.data);
+  });
+}
+
+main().catch(e => console.error(e))
