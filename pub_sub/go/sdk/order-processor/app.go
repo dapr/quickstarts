22a3dc3f12c7840a4e9ebaed031fda4e40a0dfd7
akhilac1 <chetlapalle.akhila@gmail.com>
2022-04-25 19:56:31 +0530

    -app-port for listen port, mm installation
    Signed-off-by: akhilac1 <chetlapalle.akhila@gmail.com>
    
    Resolve PR comments
    
    Signed-off-by: akhilac1 <chetlapalle.akhila@gmail.com>
    
    Update pub_sub/go/sdk/order-processor/app.go
    
    Co-authored-by: Shubham Sharma <shubhash@microsoft.com>
    Signed-off-by: akhilac1 <chetlapalle.akhila@gmail.com>

--- pub_sub/go/sdk/order-processor/app.go
+++ pub_sub/go/sdk/order-processor/app.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/dapr/go-sdk/service/common"
 	daprd "github.com/dapr/go-sdk/service/http"
@@ -18,7 +19,15 @@ var sub = &common.Subscription{
 }
 
 func main() {
-	s := daprd.NewService(":6001")
+	// read app-port passed through 'dapr run' command line
+	// Refer to https://docs.dapr.io/reference/cli/dapr-run/
+	// for dapr flags and their corresponding environment variables
+	appPort, isSet := os.LookupEnv("APP_PORT")
+	if !isSet {
+		log.Fatalf("--app-port is not set. Re-run dapr run with -p or --app-port.\nUsage: https://docs.dapr.io/getting-started/quickstarts/pubsub-quickstart/\n")
+	}
+
+	s := daprd.NewService(":" + appPort)
 	http.HandleFunc("/orders", handleRequest)
 	if err := s.AddTopicEventHandler(sub, eventHandler); err != nil {
 		log.Fatalf("error adding topic subscription: %v", err)
