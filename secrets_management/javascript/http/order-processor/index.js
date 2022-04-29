8350b0e09f1ef7aa05d5f1708f917025a1426fb6
amulyavarote <91495930+amulyavarote@users.noreply.github.com>
2022-04-29 15:15:25 -0700

    Changed variables in index.js (#665)
    Signed-off-by: Amulya Varote <amulyavarote@microsoft.com>

--- secrets_management/javascript/http/order-processor/index.js
+++ secrets_management/javascript/http/order-processor/index.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
-const DAPR_HOST = process.env.DAPR_HOST || "http://localhost";
-const DAPR_HTTP_PORT = process.env.DAPR_HTTP_PORT || "3500";
+const base_url = process.env.DAPR_HOST || "http://localhost" + ':' + process.env.DAPR_HTTP_PORT || "3500" 
 const DAPR_SECRET_STORE = "localsecretstore";
 const SECRET_NAME = "secret";
 
 async function main() {
   // Get secret from a local secret store
-  const secret = await axios.get(`${DAPR_HOST}:${DAPR_HTTP_PORT}/v1.0/secrets/${DAPR_SECRET_STORE}/${SECRET_NAME}`);
+  const secret = await axios.get(`${base_url}/v1.0/secrets/${DAPR_SECRET_STORE}/${SECRET_NAME}`);
   console.log("Fetched Secret: ", secret.data);
 }
 
