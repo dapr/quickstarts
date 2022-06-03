4c94d026145b472ba4b0659d4fda0b757b907a18
Paul Yuknewicz <paulyuk@microsoft.com>
2022-06-03 15:03:25 -0700

    Sets APP_PORT in env var of kubernetes yaml and sets default value in code if not set.  Fixes #682
    Signed-off-by: Paul Yuknewicz <paulyuk@microsoft.com>

--- tutorials/hello-kubernetes/node/app.js
+++ tutorials/hello-kubernetes/node/app.js
@@ -19,16 +19,12 @@ const app = express();
 app.use(bodyParser.json());
 
 // These ports are injected automatically into the container.
-const daprPort = process.env.DAPR_HTTP_PORT; 
-const daprGRPCPort = process.env.DAPR_GRPC_PORT;
+const daprPort = process.env.DAPR_HTTP_PORT ?? "3500"; 
+const daprGRPCPort = process.env.DAPR_GRPC_PORT ?? "50001";
 
 const stateStoreName = `statestore`;
 const stateUrl = `http://localhost:${daprPort}/v1.0/state/${stateStoreName}`;
-const port = process.env.APP_PORT ;
-if(!port) {
-    console.error('[error]: --app-port is not set. Re-run dapr run with -p or --app-port.\nUsage: https://github.com/dapr/quickstarts/tree/master/tutorials/hello-kubernetes\n');
-    process.exit(1);
-}
+const port = process.env.APP_PORT ?? "3000";
 
 app.get('/order', async (_req, res) => {
     try {
