8b404dfe9a4a5a807bccd87625e71ddbb75024e2
Paul Yuknewicz <paulyuk@microsoft.com>
2022-06-21 15:33:13 -0700

    WIP on Java
    Signed-off-by: Paul Yuknewicz <paulyuk@microsoft.com>

--- bindings/java/sdk/batch/src/main/java/com/service/controller/BatchProcessingServiceController.java
+++ bindings/java/sdk/batch/src/main/java/com/service/controller/BatchProcessingServiceController.java
@@ -6,11 +6,22 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.dapr.client.DaprClient;
+import io.dapr.client.DaprClientBuilder;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import reactor.core.publisher.Mono;
 
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.io.File;
 import java.io.IOException;
 import org.springframework.core.io.ClassPathResource;
@@ -28,35 +39,67 @@ public class BatchProcessingServiceController {
         
         logger.info("Processing batch..");
 
-        //File resource = new ClassPathResource("orders.json").getFile();
-        //logger.info(resource.getAbsolutePath());
-        //File resource = new ClassPathResource("../../../orders.json").getFile();
-        //File file = ResourceUtils.getFile("classpath:orders.json");
+        Orders ordList = this.loadOrdersFromFile("orders.json");
 
-        // return Mono.fromSupplier(() -> {
-            //     try {
-                //         logger.info("Subscriber received: " + cloudEvent.getData().getOrderId());
-                //         return ResponseEntity.ok("SUCCESS");
-                //     } catch (Exception e) {
-                    //         throw new RuntimeException(e);
-                    //     }
-                    // });
+        for (Order order : ordList.orders) {
+            String sqlText = String.format(
+                "insert into orders (orderid, customer, price) " +
+                "values (%s, '%s', %s);", 
+                order.orderid, order.customer, order.price);
+            logger.info(sqlText);
+
+            //Map<String, String> command = new HashMap<String, String>();
+            //command.put("sql", sqlText);
+            String command = String.format("{\"sql\": \"%s\"}", sqlText);
+            logger.info(command);
+            DaprClient client = new DaprClientBuilder().build();
+            client.invokeBinding(sqlBindingName, "exec", command).block();
+        }
+
         logger.info("Finished processing batch");
 
         return ResponseEntity.ok("Finished processing batch");
+    }
+
+    private Orders loadOrdersFromFile(String path) throws JsonProcessingException {
+        // this is a mock to get things running temporarily
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+
+        String json = String.join(System.getProperty("line.separator"), 
+        "{\"orders\": [",
+        "{\"orderid\": 1, \"customer\": \"John Smith\", \"price\": 100.32},",
+        "{\"orderid\": 2, \"customer\": \"Jane Bond\", \"price\": 15.4},",
+        "{\"orderid\": 3, \"customer\": \"Tony James\", \"price\": 35.56}",
+        "]",
+        "}");
+
+        Orders newOrdersBatch;
+
+        try 
+        {
+            newOrdersBatch = mapper.readValue(json, Orders.class);
+
+        } catch (Exception e) {
+            logger.error(e.toString());
+            newOrdersBatch = new Orders();
+        }
+
+        return newOrdersBatch;
     }
 }
             
 @Getter
 @Setter
 class Order {
-    private int orderid;
-    private String customer;
-    private float price;
+    public int orderid;
+    public String customer;
+    public float price;
 }
 
 @Getter
 @Setter
 class Orders {
-    private Order[] orders;
+    public List<Order> orders;
 }
