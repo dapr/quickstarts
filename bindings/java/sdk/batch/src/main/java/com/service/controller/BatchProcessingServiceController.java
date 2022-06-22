44886516ed613f9a2a390561c5c5aab99ab35ed9
Paul Yuknewicz <paulyuk@microsoft.com>
2022-06-22 16:38:15 -0700

    pretty formatted POM and java so better with merges in future
    Signed-off-by: Paul Yuknewicz <paulyuk@microsoft.com>

--- bindings/java/sdk/batch/src/main/java/com/service/controller/BatchProcessingServiceController.java
+++ bindings/java/sdk/batch/src/main/java/com/service/controller/BatchProcessingServiceController.java
@@ -1,3 +1,15 @@
+/*
+Copyright 2021 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package com.service.controller;
 
 import lombok.Getter;
@@ -18,7 +30,6 @@ import org.slf4j.LoggerFactory;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.io.IOException;
 import java.io.InputStream;
 
 
@@ -30,7 +41,7 @@ public class BatchProcessingServiceController {
     private static final String sqlBindingName = "sqldb";
     
     @PostMapping(path = cronBindingPath, consumes = MediaType.ALL_VALUE)
-    public ResponseEntity<String> processBatch() throws IOException, Exception {
+    public ResponseEntity<String> processBatch() throws Exception {
         
         logger.info("Processing batch..");
 
