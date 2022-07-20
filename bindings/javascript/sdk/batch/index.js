bfb721e0c11a4f61c53ce379cf52601b7ac75dab
Nick Greenfield <nigreenf@microsoft.com>
2022-07-20 16:35:16 -0700

    Change env var from SERVER_PORT to APP_PORT
    Signed-off-by: Nick Greenfield <nigreenf@microsoft.com>

--- bindings/javascript/sdk/batch/index.js
+++ bindings/javascript/sdk/batch/index.js
@@ -24,7 +24,7 @@ const postgresBindingName = "sqldb";
 const daprHost = process.env.DAPR_HOST || 'http://localhost';
 const daprPort = process.env.DAPR_HTTP_PORT || '3500';
 const serverHost = "127.0.0.1";
-const serverPort = process.env.SERVER_PORT || '5002';
+const serverPort = process.env.APP_PORT || '5002';
 
 const client = new DaprClient(daprHost, daprPort);
 
