c8751f5d8a970a75b6a7fa28657eaec2fffebd14
Paul Yuknewicz <paulyuk@Pauls-MBP-2.lan>
2023-02-01 10:14:56 -0800

    Modernized to 100% minimal API code; no Startup.cs needed.
    Signed-off-by: Paul Yuknewicz <paulyuk@Pauls-MBP-2.lan>

--- actors/csharp/sdk/smartdevice.Service/Program.cs
+++ actors/csharp/sdk/smartdevice.Service/Program.cs
@@ -1,40 +1,45 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿var builder = WebApplication.CreateBuilder(args);
 
-var builder = WebApplication.CreateBuilder(args);
-
-var startup = new smartdevice.Startup(builder.Configuration);
-
-builder.Services.AddOptions(); //try this
-
-startup.ConfigureServices(builder.Services);
+builder.Services.AddActors(options =>
+{
+    // Register actor types and configure actor settings
+    options.Actors.RegisterActor<smartdevice.SmokeDetectorActor>();
+});
 
 var app = builder.Build();
 
-startup.Configure(app, app.Environment);
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    // By default, ASP.Net Core uses port 5000 for HTTP. The HTTP
+    // redirection will interfere with the Dapr runtime. You can
+    // move this out of the else block if you use port 5001 in this
+    // example, and developer tooling (such as the VSCode extension).
+    app.UseHttpsRedirection();
+}
+
+app.MapActorsHandlers();
 
 app.Run();
 
-// var builder = WebApplication.CreateBuilder();
 
-// builder.Services.AddActors(options =>
-// {
-//     // Register actor types and configure actor settings
-//     options.Actors.RegisterActor<smartdevice.SmokeDetectorActor>();
-// });
+// **The following deprecated code can be removed -- which uses Startup class to configure middlware:
+
+// using Microsoft.AspNetCore.Builder;
+
+// var builder = WebApplication.CreateBuilder(args);
+
+// var startup = new smartdevice.Startup(builder.Configuration);
+
+// builder.Services.AddOptions(); //try this
+
+// startup.ConfigureServices(builder.Services);
 
 // var app = builder.Build();
 
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseDeveloperExceptionPage();
-// }
-// else
-// {
-//     // By default, ASP.Net Core uses port 5000 for HTTP. The HTTP
-//     // redirection will interfere with the Dapr runtime. You can
-//     // move this out of the else block if you use port 5001 in this
-//     // example, and developer tooling (such as the VSCode extension).
-//     app.UseHttpsRedirection();
-// }
+// startup.Configure(app, app.Environment);
 
-// app.MapActorsHandlers();
+// app.Run();
--- actors/csharp/sdk/smartdevice.Service/Startup.cs
+++ actors/csharp/sdk/smartdevice.Service/Startup.cs
@@ -1,51 +1,52 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+// **The following deprecated code can be removed -- see Program.cs minimal API code for Startup configuration
+// using Microsoft.AspNetCore.Builder;
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.Extensions.DependencyInjection;
+// using Microsoft.Extensions.Hosting;
 
-namespace smartdevice
-{
-    public class Startup
-    {
+// namespace smartdevice
+// {
+//     public class Startup
+//     {
 
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+//         public Startup(IConfiguration configuration)
+//         {
+//             Configuration = configuration;
+//         }
 
-        public IConfiguration Configuration { get; }
+//         public IConfiguration Configuration { get; }
 
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddActors(options =>
-            {
-                // Register actor types and configure actor settings
-                options.Actors.RegisterActor<SmokeDetectorActor>();
-            });
-        }
+//         public void ConfigureServices(IServiceCollection services)
+//         {
+//             services.AddActors(options =>
+//             {
+//                 // Register actor types and configure actor settings
+//                 options.Actors.RegisterActor<SmokeDetectorActor>();
+//             });
+//         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // By default, ASP.Net Core uses port 5000 for HTTP. The HTTP
-                // redirection will interfere with the Dapr runtime. You can
-                // move this out of the else block if you use port 5001 in this
-                // example, and developer tooling (such as the VSCode extension).
-                app.UseHttpsRedirection();
-            }
+//         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//         {
+//             if (env.IsDevelopment())
+//             {
+//                 app.UseDeveloperExceptionPage();
+//             }
+//             else
+//             {
+//                 // By default, ASP.Net Core uses port 5000 for HTTP. The HTTP
+//                 // redirection will interfere with the Dapr runtime. You can
+//                 // move this out of the else block if you use port 5001 in this
+//                 // example, and developer tooling (such as the VSCode extension).
+//                 app.UseHttpsRedirection();
+//             }
 
-            app.UseRouting();
+//             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                // Register actors handlers that interface with the Dapr runtime.
-                endpoints.MapActorsHandlers();
-            });
-        }
-    }
-}
+//             app.UseEndpoints(endpoints =>
+//             {
+//                 // Register actors handlers that interface with the Dapr runtime.
+//                 endpoints.MapActorsHandlers();
+//             });
+//         }
+//     }
+// }
