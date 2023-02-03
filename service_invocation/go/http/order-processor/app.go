b91d817ca50bba524e2c377aeab2adabf100bb09
Alessandro (Ale) Segala <43508+ItalyPaleAle@users.noreply.github.com>
2023-02-02 16:31:32 -0800

    Fixes some Go samples (#782)
    * Fixes service invocation Go samples
    
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
    
    * Fixes + updates to the pubsub sdk apps too
    
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
    
    * More updates
    
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>

--- service_invocation/go/http/order-processor/app.go
+++ service_invocation/go/http/order-processor/app.go
@@ -12,17 +12,24 @@ import (
 func getOrder(w http.ResponseWriter, r *http.Request) {
 	data, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Fatal(err)
+		log.Println("Error reading body:", err.Error())
 	}
-	fmt.Println("Order received : ", string(data))
+	fmt.Println("Order received:", string(data))
 	_, err = w.Write(data)
 	if err != nil {
-		log.Println("Error in writing the result obj")
+		log.Println("Error writing the response:", err.Error())
 	}
 }
 
 func main() {
+	// Create a new router and respond to POST /orders requests
 	r := mux.NewRouter()
 	r.HandleFunc("/orders", getOrder).Methods("POST")
-	_ = http.ListenAndServe(":6001", r)
+
+	// Start the server listening on port 6001
+	// This is a blocking call
+	err := http.ListenAndServe(":6001", r)
+	if err != http.ErrServerClosed {
+		log.Println("Error starting HTTP server")
+	}
 }
