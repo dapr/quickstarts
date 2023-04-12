0dc56620c19212e858df3469332953a296cbfa9e
Wralith <wralithdev@gmail.com>
2023-04-12 05:07:27 +0300

    Update javascript sdk to 3.0.0
    Signed-off-by: Ayberk Caliskan <wralithdev@gmail.com>
    Signed-off-by: Wralith <wralithdev@gmail.com>

--- secrets_management/javascript/sdk/order-processor/index.js
+++ secrets_management/javascript/sdk/order-processor/index.js
@@ -1,12 +1,16 @@
-import { DaprClient, CommunicationProtocolEnum } from '@dapr/dapr'; 
+import { DaprClient, CommunicationProtocolEnum } from '@dapr/dapr';
 
-const DAPR_HOST = process.env.DAPR_HOST || "http://localhost";
-const DAPR_HTTP_PORT = process.env.DAPR_HTTP_PORT || "3500";
+const daprHost = process.env.DAPR_HOST || "http://localhost";
+const daprPort = process.env.DAPR_HTTP_PORT || "3500";
 const DAPR_SECRET_STORE = "localsecretstore";
 const SECRET_NAME = "secret";
 
 async function main() {
-    const client = new DaprClient(DAPR_HOST, DAPR_HTTP_PORT, CommunicationProtocolEnum.HTTP);
+    const client = new DaprClient({
+        daprHost,
+        daprPort,
+        communicationProtocol: CommunicationProtocolEnum.HTTP,
+    });
     const secret = await client.secret.get(DAPR_SECRET_STORE, SECRET_NAME);
     console.log("Fetched Secret: " + JSON.stringify(secret));
 }
