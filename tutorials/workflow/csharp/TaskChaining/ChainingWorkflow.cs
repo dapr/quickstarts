3ce6f189eb7a1e5ab174266095d0196cc0680745
Marc Duiker <marcduiker@users.noreply.github.com>
2025-03-18 19:44:53 +0100

    Add C# TaskChaining
    Signed-off-by: Marc Duiker <marcduiker@users.noreply.github.com>

---
+++ tutorials/workflow/csharp/TaskChaining/ChainingWorkflow.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapr.Workflow;
+using TaskChaining.Activities;
+
+namespace TaskChaining
+{
+    public class ChainingWorkflow : Workflow<string, string>
+    {
+        public override async Task<string> RunAsync(WorkflowContext context, string input)
+        {
+            var result1 = await context.CallActivityAsync<string>(
+                nameof(Activity1),
+                input);
+            var result2 = await context.CallActivityAsync<string>(
+                nameof(Activity2),
+                result1);
+            var workflowResult = await context.CallActivityAsync<string>(
+                nameof(Activity3),
+                result2);
+
+            return workflowResult;
+        }
+    }
+}
