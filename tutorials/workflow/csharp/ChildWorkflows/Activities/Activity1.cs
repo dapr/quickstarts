b2d70f1df27b3dedb99149014bcaf553de7d806b
Marc Duiker <marcduiker@users.noreply.github.com>
2025-03-20 12:14:54 +0100

    Add C# ChildWorkflows
    Signed-off-by: Marc Duiker <marcduiker@users.noreply.github.com>

---
+++ tutorials/workflow/csharp/ChildWorkflows/Activities/Activity1.cs
@@ -0,0 +1,12 @@
+using Dapr.Workflow;
+
+namespace ChildWorkflows.Activities;
+
+public class Activity1 : WorkflowActivity<string, string>
+{
+    public override Task<string> RunAsync(WorkflowActivityContext context, string input)
+    {
+        Console.WriteLine($"{nameof(Activity1)}: Received input: {input}.");
+        return Task.FromResult($"{input} is processed" );
+    }
+}
