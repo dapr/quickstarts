757d318d8a26dcbf6f84c7f03b51ec7eb0af7ada
Marc Duiker <marcduiker@users.noreply.github.com>
2025-05-06 15:42:26 +0200

    Add fundamentals example for Python
    Signed-off-by: Marc Duiker <marcduiker@users.noreply.github.com>

---
+++ tutorials/workflow/python/fundamentals/basic/app.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, status
+from dapr.ext.workflow import DaprWorkflowClient
+from basic_workflow import wf_runtime, basic_workflow
+import uvicorn
+
+app = FastAPI()
+
+@app.post("/start/{input}", status_code=status.HTTP_202_ACCEPTED)
+async def start_workflow(input: str):
+    """
+    The DaprWorkflowClient is the API to manage workflows.
+    Here it is used to schedule a new workflow instance.
+    """
+    wf_client = DaprWorkflowClient()
+    instance_id = wf_client.schedule_new_workflow(
+            workflow=basic_workflow,
+            input=input
+        )
+    return {"instance_id": instance_id}
+
+if __name__ == "__main__":
+    wf_runtime.start()
+    uvicorn.run(app, host="0.0.0.0", port=5254)
