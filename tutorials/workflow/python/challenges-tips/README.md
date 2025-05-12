4c71f907a3910b6567dcfb5a8dccb768ec63b630
Marc Duiker <marcduiker@users.noreply.github.com>
2025-05-12 14:51:36 +0200

    Add challenges and tips code
    Signed-off-by: Marc Duiker <marcduiker@users.noreply.github.com>
    Signed-off-by: siri-varma <siri.varma@outlook.com>

---
+++ tutorials/workflow/python/challenges-tips/README.md
@@ -0,0 +1,10 @@
+# Workflow Challenges & Tips
+
+Workflow systems are very powerful tools but also have their challenges & limitations as described in the [Dapr docs](https://docs.dapr.io/developing-applications/building-blocks/workflow/workflow-features-concepts/#limitations).
+
+This section provides some tips with code snippets to understand the limitations and get the most out of the Dapr Workflow API. Read through the following examples to learn best practices to develop Dapr workflows.
+
+- [Deterministic workflows](deterministic_workflow.py)
+- [Idempotent activities](idempotent_activity.py)
+- [Versioning workflows](versioning_workflow.py)
+- [Workflow & activity payload size](payload_size_workflow.py)
---
+++ tutorials/workflow/python/challenges-tips/versioning_workflow.py
@@ -0,0 +1,32 @@
+import dapr.ext.workflow as wf
+
+wf_runtime = wf.WorkflowRuntime()
+
+"""
+This is the initial version of the workflow.
+Note that the input argument for both activities is the orderItem (string).
+"""
+@wf_runtime.workflow(name='versioning_workflow_1')
+def versioning_workflow_1(ctx: wf.DaprWorkflowContext, order_item: str):
+    result_a = yield ctx.call_activity(activity_a, input=order_item)
+    result_b = yield ctx.call_activity(activity_b, input=order_item)
+    
+    return result_a + result_b
+
+"""
+This is the updated version of the workflow.
+The input for activity_b has changed from orderItem (string) to resultA (int).
+If there are in-flight workflow instances that were started with the previous version
+of this workflow, these will fail when the new version of the workflow is deployed 
+and the workflow name remains the same, since the runtime parameters do not match with the persisted state.
+It is recommended to version workflows by creating a new workflow class with a new name:
+{workflowname}_1 -> {workflowname}_2
+Try to avoid making breaking changes in perpetual workflows (that use the `continue_as_new` method) 
+since these are difficult to replace with a new version.
+"""
+@wf_runtime.workflow(name='versioning_workflow_2')
+def versioning_workflow_2(ctx: wf.DaprWorkflowContext, order_item: str):
+    result_a = yield ctx.call_activity(activity_a, input=order_item)
+    result_b = yield ctx.call_activity(activity_b, input=result_a)
+    
+    return result_a + result_b
