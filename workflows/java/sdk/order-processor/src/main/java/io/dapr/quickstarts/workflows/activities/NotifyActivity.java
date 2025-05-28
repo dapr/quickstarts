dc2fc80e76276a4840799cb267bae2d6624615a4
Siri Varma Vegiraju <svegiraju@microsoft.com>
2025-05-28 08:31:56 -0700

    Fix imports
    Signed-off-by: Siri Varma Vegiraju <svegiraju@microsoft.com>
    Signed-off-by: salaboy <Salaboy@gmail.com>

--- workflows/java/sdk/order-processor/src/main/java/io/dapr/quickstarts/workflows/activities/NotifyActivity.java
+++ workflows/java/sdk/order-processor/src/main/java/io/dapr/quickstarts/workflows/activities/NotifyActivity.java
@@ -1,11 +1,11 @@
 package io.dapr.quickstarts.workflows.activities;
 
+import io.dapr.workflows.WorkflowActivity;
+import io.dapr.workflows.WorkflowActivityContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.dapr.quickstarts.workflows.models.Notification;
-import io.dapr.workflows.runtime.WorkflowActivity;
-import io.dapr.workflows.runtime.WorkflowActivityContext;
 
 public class NotifyActivity implements WorkflowActivity {
   private static Logger logger = LoggerFactory.getLogger(NotifyActivity.class);
