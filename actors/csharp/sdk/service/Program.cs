25144245db9e4814d27778475ebdbfd0a364bb32
Whit Waldo <whit.waldo@innovian.net>
2025-10-09 15:56:08 -0500

    HTTPS redirection is a known issue with the Dapr SDK
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

--- actors/csharp/sdk/service/Program.cs
+++ actors/csharp/sdk/service/Program.cs
@@ -14,19 +14,6 @@ builder.Services.AddActors(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-else
-{
-    // By default, ASP.Net Core uses port 5000 for HTTP. The HTTP
-    // redirection will interfere with the Dapr runtime. You can
-    // move this out of the else block if you use port 5001 in this
-    // example, and developer tooling (such as the VSCode extension).
-    app.UseHttpsRedirection();
-}
-
 app.MapActorsHandlers();
 
 app.Run();
